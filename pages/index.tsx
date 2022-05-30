99b7bd38417a88d703f57327c50a9e4b2200e43b
Mutesa-Cedric <mutesacedric@gmail.com>
2022-05-30 22:09:49 +0200

    finished welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="h-screen">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-small.png" />
       </Head>
-      <main className='w-full h-screen overflow-hidden'>
+      <main className='w-full h-full'>
         {/* nav bar */}
-        <nav className="bg-white h-16 overflow-hidden flex  shadow-md px-12 w-full">
+        <nav className="bg-white h-20 overflow-hidden flex  shadow-md lg:px-12 md:px-8 pr-4 pl-3 w-full">
           <div className="flex items-center justify-between w-full">
             <div>
               <Image
@@ -22,15 +22,15 @@ const Home: NextPage = () => {
               />
             </div>
             <div className='flex space-x-12'>
-              <button className='bg-green-500 text-gray-200 px-5 capitalize text-lg  rounded-md py-1'>register</button>
-              <button className='bg-green-500 text-gray-200 px-5 capitalize text-lg  rounded-md py-1'>login</button>
+              <button className='bg-green-200 text-green-700 hover:text-white hover:bg-green-700 px-5 capitalize text-lg  rounded-md py-1'>register</button>
+              <button className='bg-green-200 text-green-700 hover:text-white hover:bg-green-700 px-5 capitalize text-lg  rounded-md py-1'>login</button>
             </div>
           </div>
         </nav>
         {/* nav bar */}
 
         {/* main */}
-        <div className='flex items-center justify-between px-16 flex-wrap md:flex-nowrap'>
+        <div className='flex items-center justify-between h-[80%] mt-4 lg:px-16 px-4 flex-wrap lg:flex-nowrap'>
           <div className='w-full'>
             <div className="sm:text-center lg:text-left">
               <h1 className="tracking-tight font-extrabold text-4xl sm:text-5xl md:text-6xl md:text-left ">
@@ -41,24 +41,29 @@ const Home: NextPage = () => {
                 At prime properties, we Ensure that our clients get properties at affordable prices. Luxury is our priority and budget is our Constrain.We have over 783829 buyers and over 207896 sellers accross the globe. Pick and choose from 1000+ properties accross
                 the globe
               </p>
-              <div className=" sm:flex sm:justify-start ">
+              <div className=" sm:flex sm:justify-start">
                 <div className="my-5">
-                  <p className="text-xl text-gray-700 mb-4">join over 37829348+ members</p>
-                  <a className="capitalize text-xl flex justify-center rounded-md bg-green-500 py-2 text-white md:px-14 md:py-3 " href="/signup">Get Started</a>
+                  <p className="text-lg capitalize text-gray-700 mb-4">join over 37829348+ members</p>
+                  <button className='bg-green-200 text-green-700 px-20 text-lg rounded-sm py-2'>Get Started</button>
                 </div>
-                <div className="my-5 ml-20">
-                  <p className="text-xl text-gray-700 mb-4">have an account?</p>
-                  <a className="capitalize text-xl flex justify-center rounded-md bg-green-200 py-2 text-green-700 md:px-14 md:py-3 " href="/login">log in</a>
+                <div className="my-5 lg:ml-20">
+                  <p className="text-lg capitalize text-gray-700 mb-4">have an account?</p>
+                  <button className='bg-green-200 text-green-700 px-14 text-lg rounded-sm py-2'>Login</button>
                 </div>
               </div>
 
             </div>
           </div>
-          <div className='w-full border-2 border-blue-400'>
-            
+          <div className='lg:w-[90%] w-full lg:mt-0 mt-8 border-[6px] relative border-gray-100 h-[420px] rounded-xl flex items-center'>
+            <Image src={'/luxury.jpg'} layout="fill" alt='house' className=' rounded-lg ' />
           </div>
         </div>
         {/* main */}
+        {/* footer */}
+          <div className="text-center text-gray-400  italic ">
+            <p>designed and developed by <b> Mutesa Cedric</b></p>
+          </div>
+        {/* footer */}
 
       </main>
     </div>
