fd3f1558bd729296119370bcf7863550ccd3d0be
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-04 22:15:55 +0200

    added data to use for test

---
+++ propertyData.ts
@@ -0,0 +1,82 @@
+const properties = [
+    {
+        id: 1,
+        name: 'Bungalow',
+        price: 100,
+        location: 'London',
+        image: '/luxury.jpg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    },
+    {
+        id: 2,
+        name: 'kigali view',
+        price: 50000,
+        location: 'Kigali',
+        image: '/home1.jpg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    },
+    {
+        id: 3,
+        name: 'Bungalow',
+        price: 100,
+        location: 'London',
+        image: '/home2.jpg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    },
+    {
+        id: 4,
+        name: 'kigali view',
+        price: 50000,
+        location: 'Kigali',
+        image: '/home3.jpg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more'
+        , onSale: true
+    },
+    {
+        id: 5,
+        name: 'Bungalow',
+        price: 100,
+        location: 'London',
+        image: '/house4.webp',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    },
+    {
+        id: 6,
+        name: 'kigali view',
+        price: 50000,
+        location: 'Kigali',
+        image: '/house5.jpeg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    }
+    ,
+    {
+        id: 7,
+        name: 'Bungalow',
+        price: 100,
+        location: 'London',
+        image: '/luxury.jpg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    },
+    {
+        id: 8,
+        name: 'kigali view',
+        price: 50000,
+        location: 'Kigali',
+        image: '/house5.jpeg',
+        description: 'House located in suburb of kigali. This house has 3 rooms, a modern kitchen and more',
+        onSale: true
+    }
+]
+
+
+
+
+export {
+    properties
+}
