f4f3fb6b6a861a4a35dd0b0e23d13235c2bd2bb9
Mutesa-Cedric <mutesacedric@gmail.com>
2022-09-24 14:37:40 +0200

    sanity init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="font-bold flex items-center justify-center h-screen w-screen">
       Hello prime properties
+      <div>
+        
+      </div>
     </div>
-
-
   )
 }
 export default Home
